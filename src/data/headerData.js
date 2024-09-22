929a3ffa6f66da39e2174bae5f12811c842a6dc3
Prantik0616 <prantik.techvill@gmail.com>
2024-09-22 16:29:49 +0600

    done portfolio

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
-
+import image from '../assets/png/nishat.jpg'
 
 export const headerData = {
     name: 'Nishat Raihana Prantik',
     title: "Software Engineer",
     desciption:"Hey there, As a software engineer, I specialize in designing, developing, and maintaining software solutions that solve complex problems. My expertise spans various programming languages, frameworks, and tools, enabling me to build scalable, efficient, and user-friendly applications. I thrive in collaborative environments, continually refining my skills to stay at the forefront of technological advancements.",
-    image: 'src/assets/png/nishat.jpg',
-    resumePdf:"https://drive.google.com/file/d/1_o2fpLilesBeTK0mZzblTM7ED_poonGo/view?usp=sharing"
+    image: image,
+    resumePdf: 'https://drive.google.com/file/d/1_o2fpLilesBeTK0mZzblTM7ED_poonGo/view?usp=sharing'
 }
